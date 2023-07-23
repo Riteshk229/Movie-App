dd2eaf29a17e923518dcda097407b8fac2196ea2
Ritesh <Riteshk229@gmail.com>
2023-07-23 22:52:29 +0530

    made NAvar and moviecard components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,28 @@
 import { useState } from 'react'
+import { data } from '../data'
+import Navbar from './Navbar'
+import MovieCard from './MovieCard'
 import '../styles/App.css'
 
 function App() {
 
   return (
     <>
-      Project Setup
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
+
+          <div className="list">
+            {data.map((movie,index) => (
+              <MovieCard movie={movie} key={index} />
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   )
 }
